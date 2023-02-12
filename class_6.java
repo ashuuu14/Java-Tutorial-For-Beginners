c34822bd1e5db0a0b300d2136921208c4779fbbe
Ayush Vishwakarma <92978122+ashuuu14@users.noreply.github.com>
2023-02-12 21:47:29 +0530

    Add files via upload

---
+++ class_6.java
@@ -0,0 +1,32 @@
+public class class_6 {
+    public static void main(String[] args) {
+
+        // types of operator
+        // 1. Arithmetic -->  {+, -, *, /, %}
+        // 2. Relational -->  {<,>,<=, >=, !=, ==}
+        // 3. Logical --> {&&, ||, !}
+        // 4. Incr/ Dncr --> {--A, A--, ++A, A++}
+        // 5. Assignment --> {=, +=, -=, *=,/=,%= etc }
+//
+//        int a=5;
+//        int b=5;
+//        int c = a+b;
+//        int d = a-b;
+//        int e = a*b;
+//        int f = a/b;
+//        int g = a%b;
+//        System.out.println(c);
+//        System.out.println(d);
+//        System.out.println(e);
+//        System.out.println(f);
+//        System.out.println(g);
+
+//        System.out.println(6==6);
+//       System.out.println( 6<7 && 6>5);
+//       System.out.println( 6<5 || 6>5);
+//        2 &
+
+
+
+    }
+}

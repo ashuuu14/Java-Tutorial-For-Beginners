7cd71e89ac2fd4d75081d81a2a9f0909776d0395
Ayush Vishwakarma <92978122+ashuuu14@users.noreply.github.com>
2023-02-12 13:32:19 +0530

    Add files via upload

---
+++ class_1.java
@@ -0,0 +1,10 @@
+public class class_1 {
+    public static void main(String[] TechCoder) {
+        System.out.println("Hello World!");
+        System.out.println("Hello");
+    }
+}
+
+//add tow number
+// AddTwoNumber --> pascal namin convention
+// addTwoNumber --> camel naming convention
---
+++ class_5.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class class_5 {
+    public static void main(String[] args) {
+//        int number = 10;
+//        byte num = 10;
+//        long l = 4545454545787L;
+//        short s = 5;
+//        char c = 't';
+//        float f = 1.2f;
+//        double d = 5.2;
+//        boolean b = true;
+//        boolean a = false;
+//        System.out.println(" The int = "+number);
+//        System.out.println("Long = "+l+" flot = "+f);
+//        System.out.println("a = "+a+" b = "+b);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:- ");
+//        float a = sc.nextFloat();
+//        System.out.println("Enter the number B:- ");
+//        float b = sc.nextFloat();
+//        float sum = a + b;
+//        System.out.println("The Ans");
+//        System.out.println(sum);
+
+//        boolean a = sc.hasNextInt();
+//        System.out.println(a);
+
+        String s = sc.nextLine();
+        System.out.println(s);
+
+    }
+}

3ce073a0da207715091ff119c690361d326e723a
Ayush Vishwakarma <92978122+ashuuu14@users.noreply.github.com>
2023-02-15 21:10:58 +0530

    Add files via upload

---
+++ practice_set_1.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class practice_set_1 {
+    public static void main(String[] args) {
+        // Ans. No 1 :-
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter The Number 1 ");
+//        int a = sc.nextInt();
+//        System.out.println("Enter The Number 2 ");
+//        int b = sc.nextInt();
+//        int add = a+b;
+//        int mul = a*b;
+//        System.out.println(a +" + "+ b +" = "+add);
+//        System.out.println(a +" x "+ b +" = "+mul);
+
+
+        // Ans no. 2:-
+
+//        Scanner sc = new Scanner(System.in);
+//        double radius = sc.nextDouble();
+//        double perimeter = 2*Math.PI*radius;
+//        double area = Math.PI*radius*radius;
+//        System.out.println("perimeter = "+perimeter);
+//        System.out.println("area = "+area);
+
+        // ans no. 3:-
+//       Scanner sc = new Scanner(System.in);
+//       float eng, phy, chem, math, comp;
+//
+//        System.out.println("Enter Marks of five subjects:- ");
+//        System.out.println("Enter Marks of English subjects");
+//        eng = sc.nextFloat();
+//        System.out.println("Enter Marks of Physics subjects");
+//        phy = sc.nextFloat();
+//        System.out.println("Enter Marks of Chemistry subjects");
+//        chem = sc.nextFloat();
+//        System.out.println("Enter Marks of Maths subjects");
+//        math = sc.nextFloat();
+//        System.out.println("Enter Marks of computer subjects");
+//        comp = sc.nextFloat();
+//
+//        double total = eng + phy + chem + math + comp;
+//        double percentage = (total / 500.0)*100;
+//
+//        System.out.println("Total Marks = "+total);
+//        System.out.println("Percentage = "+percentage);
+
+        // Ans no 4:-
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * * ");
+        System.out.println("* * * * * ");
+        System.out.println("* * * * * *");
+
+    }
+}

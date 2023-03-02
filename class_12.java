3509d704e3d66ff66a19ba5e1f0ecd0758d85ccb
Ayush Vishwakarma <92978122+ashuuu14@users.noreply.github.com>
2023-03-02 15:04:46 +0530

    Add files via upload

---
+++ class_12.java
@@ -0,0 +1,64 @@
+import java.util.Locale;
+
+public class class_12 {
+    public static void main(String[] args) {
+        //String s = "Techcoder";
+        // 1. Length.
+//        int num = s.length();
+        // 2. toUpperCase.
+        // 3. toLowercase.
+//        String s1 = s.toUpperCase();
+//        String s2 = s.toLowerCase();
+//        System.out.println(s1);
+//        System.out.println(" Lower Case :- "+s2);
+
+        // 4. trim
+//        String string = "        Techcoder";
+//        System.out.println(string);
+//        String string1 = string.trim();
+//        System.out.println(string1);
+
+        // 5. Substring.
+//        String s = "techcoder";
+//        System.out.println(s.substring(3));
+//        System.out.println(s.substring(1,8));
+
+        // 6. replace.
+
+//        String s = "Coderr";
+//        System.out.println(s.replace('C','s'));
+//        System.out.println(s.replace("Coderr","tech"));
+
+        // 7. startsWith
+//        String s = "Techcoder";
+//        System.out.println(s.startsWith("coder"));
+
+        // 8. endsWith
+
+//        String s = "Techcoder";
+//        System.out.println(s.endsWith("er"));
+
+        // 9. chatAt
+//        String s = "Techcoder";
+//        System.out.println(s.charAt(3));
+
+        // 10. indexOf
+//        String s = "Techcoder";
+//        System.out.println(s.indexOf("z"));
+
+
+       // 11. lastIndexOf
+//        String s = "Techcoder";
+//        System.out.println(s.lastIndexOf("c"));
+
+        // 12. equals
+        String s = "Techcoder";
+        System.out.println(s.equals("techcoder"));
+        System.out.println(s.equalsIgnoreCase("techCoder"));
+
+
+
+        // T e c h c o d e  r
+        // 0 1 2 3 4 5 6 7 8
+    }
+}

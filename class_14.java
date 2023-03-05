a14c9366ba80800b0e4e9db152c3c136c24bf0f6
Ayush Vishwakarma <92978122+ashuuu14@users.noreply.github.com>
2023-03-06 01:38:36 +0530

    Add files via upload

---
+++ class_14.java
@@ -0,0 +1,29 @@
+public class class_14 {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+//        if (a==5 && b==5){
+//            System.out.println("True");
+//        }else {
+//            System.out.println("False");
+//        }
+//         if (a==5 || b==5){
+//             System.out.println("true");
+//         }else {
+//             System.out.println("false");
+//         }
+
+//        if (a!=b){
+//            System.out.println(true);
+//        }else {
+//            System.out.println(false);
+//        }
+
+        System.out.println("Value of A ");
+        System.out.println(!a);
+        System.out.println("Value of B ");
+        System.out.println(!b);
+
+
+    }
+}
